d1ab2fbf5aa01b246bf47b5544184cc7b2a4fcc7
root <root@DESKTOP-C5EOE5B>
2025-05-09 13:23:54 +0100

    fault in selectexhibitions function

--- db/test-db/test-data/exhibitions.js
+++ db/test-db/test-data/exhibitions.js
@@ -14,6 +14,14 @@ module.exports = [
       description: "A collection highlighting geometry and structure in built environments.",
       created_at: "2025-01-22T08:45:00Z",
       updated_at: "2025-01-22T08:45:00Z"
-    }
+    },
+    {
+        id: 3,
+        user_id: 2,
+        title: "Trees",
+        description: "A collection of trees.",
+        created_at: "2025-01-22T08:45:00Z",
+        updated_at: "2025-01-22T08:45:00Z"
+      }
   ];
   
